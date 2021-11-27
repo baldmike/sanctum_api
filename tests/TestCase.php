036178e66cb7bd48483f2473c2dafc73eec575e1
Bald Mike <baldmike312@gmail.com>
2021-11-27 16:50:50 -0600

    adds testcase with utility methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,53 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    
+    /** @var string */
+    protected $token = '';
+
+    /**
+     * Log in random admin user
+     *
+     * @return object
+     */
+    public function loginRandomUser()
+    {
+        $user = User::inRandomOrder()->first();
+
+        $tokenData = [
+            'grant_type' => 'password',
+            'email' => trim($user->email),
+            'password' => 'password',
+            'scope' => ''
+        ];
+
+        $response = $this->json('POST', '/api/login', $tokenData);
+
+        $json = json_decode($response->content());
+
+        if($response->getStatusCode() == 200)
+        {
+            $this->token = $json->token;
+        }
+
+        return $response;
+    }
+
+    public function logoutUser()
+    {
+        $response = $this->json('POST', 'api/logout');
+
+        return $response;
+    }
+
+    public function get_current_user()
+    {
+        return Auth::user();
+    }
 }
