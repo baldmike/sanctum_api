2d4d983cdae1fb8ec054ab7c34296d50f2334d59
Bald Mike <baldmike312@gmail.com>
2021-11-27 17:49:28 -0600

    require email type

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -25,7 +25,7 @@ class UserRegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string',
-            'email' => 'required|string|unique:users,email',
+            'email' => 'required|string|email|unique:users,email',
             'password' => 'required|string|confirmed'
         ];
     }
