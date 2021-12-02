5bd6df3096ebef6d6ad20506a58e44f5559db890
Bald Mike <baldmike312@gmail.com>
2021-12-01 19:14:46 -0600

    clean up

--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -26,7 +26,7 @@ class UserRegisterRequest extends FormRequest
         return [
             'name' => 'required|string',
             'email' => 'required|string|email|unique:users,email',
-            'password' => 'required|string|confirmed'
+            'password' => 'required|string|confirmed',
         ];
     }
 }
